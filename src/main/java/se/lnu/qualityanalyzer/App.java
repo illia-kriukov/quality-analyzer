4b3733b29603e01a678f043e79d00f59db857dfe
Ilya Kriukov <ilyakriukov@icloud.com>
2017-11-05 19:46:26 +0100

    Created initial project structure, edited .gitignore file

---
+++ src/main/java/se/lnu/qualityanalyzer/App.java
@@ -0,0 +1,13 @@
+package se.lnu.qualityanalyzer;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
